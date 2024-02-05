d56901fc3920980b52285e46930aaf46661a9098
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-02-05 14:25:27 -0800

    updated files extention during the class call

--- script.mjs
+++ script.mjs
@@ -1,7 +1,7 @@
 // ======== data fetching from external sources and modules and imports ========
 
 // Importing database functions. DO NOT MODIFY THIS LINE.
-import { central, db1, db2, db3, vault } from "./databases.js";
+import { central, db1, db2, db3, vault } from "./databases.mjs";
 
 function getUserData(id) {
   const dbs = {
@@ -10,3 +10,4 @@ function getUserData(id) {
     db3: db3
   };
 }
+
