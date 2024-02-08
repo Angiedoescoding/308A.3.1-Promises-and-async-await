1d7962d85ced7c09f2d863fda29081e48d0c308a
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-02-07 18:24:06 -0800

    lab after the teacher

--- script.mjs
+++ script.mjs
@@ -3,24 +3,65 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.mjs";
 
-function getUserData(id) {
+function getUserData1(id) {
   const dbs = {
     db1: db1,
     db2: db2,
-    db3: db3
+    db3: db3,
   };
+  let getdb = Promise.resolve(central(id));
+  let resultObj = {};
+  getdb
+    .then((data) => {
+      console.log(data);
+      let db = Promise.resolve(dbs[data](id));
+      db.then((dbdata) => {
+        // console.log(dbdata);
+        resultObj.id = id;
+        resultObj.username = dbdata.username;
+        resultObj.website = dbdata.website;
+        resultObj.company = dbdata.company;
+        console.log("Result: ", resultObj);
+      }).catch((err) => {
+        console.log("Error: ", err);
+      });
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+    });
+  let vaultData = Promise.resolve(vault(id));
+  vaultData.then((vdata) => {
+    // console.log("Vault Data:", vdata);
+    resultObj.name = vdata.name;
+    resultObj.email = vdata.email;
+    resultObj.address = vdata.address;
+    resultObj.phone = vdata.phone;
+    console.log("Result: ", resultObj);
+  });
 }
-
-// ˆ^initial data
-
-// checking below:
-
-// const id = 3
-// getUserData(id)
-//   .then(userData => console.log(userData))
-//   .catch(error => console.log(error))
-
-
-// this does not work
-
-//testing git from a new loc
+// getUserData1(5);
+// Second method - async/await
+async function getUserData2(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3,
+  };
+  let getdb = await central(id);
+  let resultObj = {};
+  console.log("Get db: ", getdb);
+  let db = await dbs[getdb](id);
+  let vaultData = await vault(id);
+  // console.log("DB: ", db);
+  // console.log("Vault: ", vaultData);
+  resultObj.id = id;
+  resultObj.username = db.username;
+  resultObj.website = db.website;
+  resultObj.company = db.company;
+  resultObj.name = vaultData.name;
+  resultObj.email = vaultData.email;
+  resultObj.address = vaultData.address;
+  resultObj.phone = vaultData.phone;
+  console.log("Result obj:", resultObj);
+}
+getUserData2(3);
